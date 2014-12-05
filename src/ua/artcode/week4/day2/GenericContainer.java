57cd82870716a8ce027c4d37168b395e87369e73
Dmitriy <dmitriynikityuk@gmail.com>
2014-12-05 09:29:02 +0200

    week4.day2 classwork committing from office

--- src/ua/artcode/week4/day2/GenericContainer.java
+++ src/ua/artcode/week4/day2/GenericContainer.java
@@ -7,31 +7,39 @@ package ua.artcode.week4.day2;
  * Time: 20:19
  * To change this template use File | Settings | File Templates.
  */
-public class GenericContainer <T, V> {
+public class GenericContainer<T,V> {
 
     private T one;
     private V two;
 
     public GenericContainer() {
     }
+
     public GenericContainer(T one, V two) {
         this.one = one;
         this.two = two;
     }
+
     public void test(){
     }
+
     public T getOne() {
         return one;
     }
+
     public void setOne(T one) {
         this.one = one;
     }
+
     public V getTwo() {
         return two;
     }
+
     public void setTwo(V two) {
         this.two = two;
     }
+
     public static void show(){
+
     }
 }
