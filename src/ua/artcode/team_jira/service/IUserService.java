ddca090d99bd62c36aeb2bfb0ac5f4ca5450c710
Dmitriy <dmitriynikityuk@gmail.com>
2014-12-07 21:16:40 +0200

    Class Work week5.day2 committing from class

---
+++ src/ua/artcode/team_jira/service/IUserService.java
@@ -0,0 +1,14 @@
+package ua.artcode.team_jira.service;
+
+import ua.artcode.team_jira.exception.RegistrationException;
+
+/**
+ *
+ */
+public interface IUserService {
+
+    void register(String login, String email, String pass) throws RegistrationException;
+
+    void login(String login, String pass);
+
+}
---
+++ src/ua/artcode/team_jira/service/IUserServiceImpl.java
@@ -0,0 +1,41 @@
+package ua.artcode.team_jira.service;
+
+import ua.artcode.team_jira.db.AppDataHolder;
+import ua.artcode.team_jira.exception.RegistrationException;
+import ua.artcode.team_jira.model.User;
+import ua.artcode.team_jira.validation.UserValidator;
+
+import java.util.List;
+
+/**
+ * Created by admin on 06.12.2014.
+ */
+public class IUserServiceImpl implements IUserService {
+
+    private UserValidator userValidator;
+
+
+    @Override
+    public void register(String login, String email, String pass)
+            throws RegistrationException{
+        List<User> userList = AppDataHolder.getINSTANCE().getUserList();
+
+        User user = new User(login, pass, email);
+        if(userList.contains(user)){
+            throw new RegistrationException("Email or login already exists in db");
+        }
+
+
+        if(userValidator.validate(user)){
+            userList.add(user);
+        } else {
+            return;
+        }
+    }
+
+    @Override
+    public void login(String login, String pass) {
+        //TODO
+        AppDataHolder.getINSTANCE().getUserList();
+    }
+}
